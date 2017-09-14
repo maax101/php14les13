8155431da41ce50f40e3cd44369f7e6d31986202
maax101 <belous.maxim@gmail.com>
2017-09-14 19:24:40 +0300

    2

--- settings.php
+++ settings.php
@@ -1,9 +1,10 @@
 <?php
 //http://university.netology.ru/u/belous/me/lesson13/lesson4_2.php
 $tasks = new PDO("mysql:host = localhost; dbname=mybase", "root", "");
+print_r($_POST);
 
 $sql   = ('SELECT * FROM tasks');
-
+var_dump($sql);
 if (isset($_POST['save']) && !empty($_POST['description'])) {
     $insert = 'INSERT INTO `tasks` (`description`, `is_done`, `date_added`)
 VALUES (:description, 0, now())';
@@ -12,7 +13,7 @@ $insert_st = $tasks->prepare($insert);
 }
 
 if (isset($_POST['sort']) && !empty($_POST['sort_by'])) {
-    $sql .= ' ORDER BY ' . $tasks->quote($_POST['sort_by'] ). ' ';
+    $tasks->quote ($sql .= ' ORDER BY ' . ($_POST['sort_by']). ' ');
 }
 
 if (isset($_GET['action']) && $_GET['action'] == 'delete') {
@@ -20,7 +21,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete') {
 $del_st = $tasks->prepare($del);
     $del_st->execute([':id' => $_GET['id']]);
 }
-
+var_dump($sql);
 function showData($tasks, $sql)
 {
     if (!empty($tasks)) {
